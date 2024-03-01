5e8a3329a6514278ff2ad4cd7f4730bbf715909b
jaydin@jaydinleeman.com <jaydin@jaydinleeman.com>
2024-03-01 15:57:42 -0500

    Test

--- app/components/CreateForm/CreateForm.tsx
+++ app/components/CreateForm/CreateForm.tsx
@@ -5,18 +5,18 @@ import {
   TextInput,
   Button,
   Box,
-  Select,
   Accordion,
   AccordionItem,
   AccordionControl,
   NativeSelect,
   AccordionPanel,
-  SimpleGrid,
+  Grid,
 } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
 import { useState } from "react";
 import { IconDevices2 } from "@tabler/icons-react";
 import accordionCSS from "./Accordion.module.css";
+import classes from "./CreateForm.module.css";
 
 export default function CreateForm() {
   const form = useForm({
@@ -79,78 +79,85 @@ export default function CreateForm() {
 
   return (
     <form onSubmit={form.onSubmit((values) => console.log(values))}>
-      <SimpleGrid cols={2}>
-        <Box ml={50}>
-          <TextInput label="Form Title" {...form.getInputProps("formTitle")} />
-          <DatePickerInput
-            label="Start Date"
-            placeholder="Pick date"
-            value={startDateValue}
-            {...form.getInputProps("startDate")}
-          />
-          <DatePickerInput
-            label="End Date"
-            placeholder="Pick date"
-            value={endDateValue}
-            {...form.getInputProps("endDate")}
-          />
-          <Button type="submit" color="gray" mt={10}>
-            Create Form
-          </Button>
-        </Box>
+      <Grid>
+        <Grid.Col span={6}>
+          <Box className={classes.box1}>
+            <TextInput
+              label="Form Title"
+              {...form.getInputProps("formTitle")}
+            />
+            <DatePickerInput
+              label="Start Date"
+              placeholder="Pick date"
+              value={startDateValue}
+              {...form.getInputProps("startDate")}
+            />
+            <DatePickerInput
+              label="End Date"
+              placeholder="Pick date"
+              value={endDateValue}
+              {...form.getInputProps("endDate")}
+            />
+            <Button type="submit" color="gray" mt={10}>
+              Create Form
+            </Button>
+          </Box>
+        </Grid.Col>
 
-        <Box mr={50}>
-          <TextInput
-            withAsterisk
-            label="Item Name"
-            value={itemForm.itemName}
-            onChange={(e) => {
-              setItemForm({ ...itemForm, itemName: e.target.value });
-            }}
-          />
-          <TextInput
-            label="Item Serial Number"
-            value={itemForm.itemSN}
-            onChange={(e) => {
-              setItemForm({ ...itemForm, itemSN: e.target.value });
-            }}
-          />
-          <TextInput
-            label="Item Asset Tag"
-            value={itemForm.itemAssetTag}
-            onChange={(e) => {
-              setItemForm({ ...itemForm, itemAssetTag: e.target.value });
-            }}
-          />
-          <NativeSelect
-            label="Category"
-            value={itemForm.itemCategory}
-            onChange={(e) => {
-              setItemForm({
-                ...itemForm,
-                itemCategory: e!.currentTarget.value,
-              });
-            }}
-            placeholder="Pick category"
-            data={[
-              "Desktop",
-              "Laptop",
-              "Server",
-              "Networking Equipment",
-              "UPS",
-              "Component",
-              "Other",
-            ]}
-          />
-          <Button color="gray" mt={10} mb={10} onClick={addNewItemToArray}>
-            + Add New Item
-          </Button>
-          <br />
-          <Accordion classNames={accordionCSS} variant="seperate" radius="xl">
-            {items}
-          </Accordion>
-        </Box>
-      </SimpleGrid>
+        <Grid.Col span={6}>
+          <Box className={classes.box2}>
+            <TextInput
+              withAsterisk
+              label="Item Name"
+              value={itemForm.itemName}
+              onChange={(e) => {
+                setItemForm({ ...itemForm, itemName: e.target.value });
+              }}
+            />
+            <TextInput
+              label="Item Serial Number"
+              value={itemForm.itemSN}
+              onChange={(e) => {
+                setItemForm({ ...itemForm, itemSN: e.target.value });
+              }}
+            />
+            <TextInput
+              label="Item Asset Tag"
+              value={itemForm.itemAssetTag}
+              onChange={(e) => {
+                setItemForm({ ...itemForm, itemAssetTag: e.target.value });
+              }}
+            />
+            <NativeSelect
+              label="Category"
+              value={itemForm.itemCategory}
+              onChange={(e) => {
+                setItemForm({
+                  ...itemForm,
+                  itemCategory: e!.currentTarget.value,
+                });
+              }}
+              placeholder="Pick category"
+              data={[
+                "Desktop",
+                "Laptop",
+                "Server",
+                "Networking Equipment",
+                "UPS",
+                "Component",
+                "Other",
+              ]}
+            />
+            <Button color="gray" mt={10} mb={10} onClick={addNewItemToArray}>
+              + Add New Item
+            </Button>
+            <br />
+            <Accordion classNames={accordionCSS} variant="seperate" radius="xl">
+              {items}
+            </Accordion>
+          </Box>
+        </Grid.Col>
+      </Grid>
     </form>
   );
 }
